bc111c4b3421bc28843552b54a1ab4be485e9ddb
ch4oSj0k3r <s.nowitzky@outlook.de>
2022-03-22 08:08:58 +0100

    add prisma and init it

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import prisma from '../../prisma/prisma'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function assetHandler(req, res) {
+  const {method} = req
+  switch (method) {
+    case 'GET':
+      try {
+        const stars = await prisma.season.findMany()
+        res.status(200).json(stars)
+      } catch (e) {
+        console.error('Request error', e)
+        res.status(500).json({error: 'Error fetching posts'})
+      }
+      break
+    default:
+      res.setHeader('Allow', ['GET'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+      break
+  }
 }
