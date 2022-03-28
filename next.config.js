229853ed9543b4dff6b8061f77c8083e85cfa0cf
ch4oSj0k3r <s.nowitzky@outlook.de>
2022-03-28 11:42:47 +0200

    disable pwa on local development

--- next.config.js
+++ next.config.js
@@ -4,9 +4,10 @@ const withPWA = require('next-pwa')
 const nextConfig = {
   reactStrictMode: true,
   pwa: {
-    dest: "public",
+    dest: 'public',
     register: true,
     skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
   },
 }
 
