9d60811a2e1a651a1273e3dd28f5091ee89f690a
ch4oSj0k3r <s.nowitzky@outlook.de>
2023-05-11 15:03:12 +0200

    add collapse to playerstat widget

--- components/Widgets/components/PlayerWidget/index.js
+++ components/Widgets/components/PlayerWidget/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useSession} from 'next-auth/react'
 
 import Widget from '../..'
@@ -7,24 +7,33 @@ import StatsButtons from '../../../StatsButtons'
 
 const PlayerWidget = ({player, setPlayers}) => {
   const {data: session} = useSession()
+  const [open, setOpen] = useState(true)
 
   if (!player) return
 
   return (
     <Widget>
-      <div className="flex items-center">
-        <div className="grow">
-          <h2 className="card-title text-primary">{`${player.firstname} ${player.lastname} (${player.playernumber})`}</h2>
-        </div>
-      </div>
-      <div>
-        {session && session.user && (
-          <div className="grid mb-4">
-            <StatsButtons player={player} setPlayers={setPlayers} />
+      <div
+        tabIndex={0}
+        className={`collapse md:collapse-open ${open ? 'collapse-open' : 'collapse-close'}`}
+      >
+        <div
+          className="flex items-center collapse-title cursor-pointer md:cursor-default"
+          onClick={() => setOpen(!open)}
+        >
+          <div className="grow">
+            <h2 className="card-title text-primary">{`${player.firstname} ${player.lastname} (${player.playernumber})`}</h2>
           </div>
-        )}
-        <div>
-          <RadarChart playerStats={player.playerStats} minify />
+        </div>
+        <div className="collapse-content">
+          {session && session.user && (
+            <div className="grid mb-4">
+              <StatsButtons player={player} setPlayers={setPlayers} />
+            </div>
+          )}
+          <div>
+            <RadarChart playerStats={player.playerStats} minify />
+          </div>
         </div>
       </div>
     </Widget>
